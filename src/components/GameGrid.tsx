331f1e5f9ddde724965143dbe0488105a0fa95b1
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2023-04-08 23:44:18 +0300

    Feature/loading skeletons on game cards (#3)
    * Introduce loading skeletons on GameCard component
    
    * Remove style duplication by creating a GameCardContainer

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,30 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 import { useGames } from "../hooks";
 
+const skeletons = Array(12)
+  .fill(undefined)
+  .map((_, i) => i + 1);
+
 export default function GameGrid() {
-  const { games, error } = useGames();
+  const { loading, games, error } = useGames();
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
         spacing={10}>
+        {loading &&
+          skeletons.map(skeleton => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
         {games.map(game => (
           <GameCard game={game} key={game.id} />
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,22 +9,28 @@ interface FetchGamesResponse {
 }
 
 export default function useGames() {
+  const [loading, setLoading] = useState(false);
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then(res => setGames(res.data.results))
+      .then(res => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch(err => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { loading, games, error };
 }
