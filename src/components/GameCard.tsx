289da5306ff20b935577c7837e1f58708950fb8f
kboul <kos.voulgaridis@gmail.com>
2023-04-08 23:26:14 +0300

    Fetch smaller images in size to improve UX and image loading

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import { getCroppedImageUrl } from "../utils";
 
 interface GameCardProps {
   game: Game;
@@ -14,7 +15,7 @@ interface GameCardProps {
 export default function GameCard({ game }: GameCardProps) {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
