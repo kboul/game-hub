344b20f0cd66915f0ec6a6def2af4af9dfe2d677
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2023-04-14 15:25:35 +0300

    Change dynamically the dark mode text depending on the selected mode to show the opposite if toggling

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -3,14 +3,16 @@ import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 export default function ColorModeSwitch() {
   const { toggleColorMode, colorMode } = useColorMode();
 
+  const isDark = colorMode === "dark";
+
   return (
     <HStack>
       <Switch
         colorScheme="green"
-        isChecked={colorMode === "dark"}
+        isChecked={isDark}
         onChange={toggleColorMode}
       />
-      <Text whiteSpace="nowrap">Dark mode</Text>
+      <Text whiteSpace="nowrap">{isDark ? "Light" : "Dark"} mode</Text>
     </HStack>
   );
 }
