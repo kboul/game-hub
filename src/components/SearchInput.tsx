d5703edd8145b3ebe0499344d195535453ec0495
kboul <kos.voulgaridis@gmail.com>
2023-12-28 16:59:14 +0200

    Fix bug with results not updating when searching the bar and we are on games detail page

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,18 +1,24 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { FormEvent, useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 import { useGameQueryStore } from "../hooks";
 
 const formStyle = { width: "100%" };
 
 export default function SearchInput() {
+  const navigate = useNavigate();
+
   const ref = useRef<HTMLInputElement>(null);
   const setSearchedGame = useGameQueryStore((state) => state.setSearchedGame);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (ref.current) setSearchedGame(ref.current.value);
+    if (ref.current) {
+      setSearchedGame(ref.current.value);
+      navigate("/");
+    }
   };
 
   return (
