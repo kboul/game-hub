fa5cbed04b5d3353c5eefd737e2f972767524915
kboul <kos.voulgaridis@gmail.com>
2023-12-28 16:39:57 +0200

    Place GameDetails content on two grids, & improve truncated description

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Box, Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  SimpleGrid,
+  Spinner,
+  Text
+} from "@chakra-ui/react";
 
 import { GameAttributes, GameScreenshots, GameTrailers } from "../components";
 import { useGameDetails } from "../hooks";
@@ -10,40 +17,43 @@ export default function GameDetailsPage() {
 
   const { data: game, isLoading, isError } = useGameDetails(id!);
 
-  const [isTruncated, setIsTruncated] = useState(true);
+  const [noOfLines, setNoOfLines] = useState(5);
 
   if (isLoading) return <Spinner />;
   if (isError || !game) return <Box>Something went wrong</Box>;
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <Text isTruncated={isTruncated}>{game.description_raw}</Text>
-      <Button
-        colorScheme="yellow"
-        fontWeight="bold"
-        onClick={() => setIsTruncated(!isTruncated)}
-        size="xs"
-        variant="solid">
-        Show {isTruncated ? "more" : "less"}
-      </Button>
-      <Box marginTop={5}>
-        <GameAttributes game={game} />
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      <Box>
+        <Heading>{game.name}</Heading>
+        <Text noOfLines={noOfLines}>{game.description_raw}</Text>
+        <Button
+          colorScheme="yellow"
+          fontWeight="bold"
+          onClick={() => setNoOfLines((prev) => (prev === 5 ? 0 : 5))}
+          size="xs"
+          variant="solid">
+          Show {noOfLines === 5 ? "more" : "less"}
+        </Button>
+
+        <Box marginTop={5}>
+          <GameAttributes game={game} />
+        </Box>
       </Box>
 
-      <Box display="flex" justifyContent="center" marginTop={5}>
-        <GameTrailers gameId={game.id} />
-      </Box>
+      <Box>
+        <Box display="flex" justifyContent="center" marginTop={5}>
+          <GameTrailers gameId={game.id} />
+        </Box>
 
-      <Box marginTop={5}>
         <GameScreenshots gameId={game.id} />
       </Box>
 
-      <Box marginTop={5}>
+      <Box>
         <Button as={Link} to="/">
           Go back
         </Button>
       </Box>
-    </>
+    </SimpleGrid>
   );
 }
